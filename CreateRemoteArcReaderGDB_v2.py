85fa38ff8f4b0e5b0708c94c0c4198fa4af0c901
Kim Sundeen <kim.h.sundeen@gmail.com>
2016-03-08 08:55:55 -0600

    Update CreateRemoteArcReaderGDB_v2.py

--- CreateRemoteArcReaderGDB_v2.py
+++ CreateRemoteArcReaderGDB_v2.py
@@ -3,7 +3,7 @@
 
 """
 VERBOSE DESCRIPTION:
-Here's the start of a python program to automate the copy & export of an SDE enterprise
+Python program automates the copy & export of an SDE enterprise
 geodatabase for use in disconnected (no internet access) remote laptop environment.
 Replication of the SDE geodatabase was not an option since the entire database was too
 large; therefore we needed to select a subset of all feature datasets and feature classes
@@ -11,6 +11,29 @@ to copy into a PortableDuluth.gdb. This python program also automates copying, c
 & export of parcel layers for ownership. The script creates a track log of all procedures
 completed with logging any errors as well as gives a script runtime.
 
+# NEED FOR ARCREADER UPDATING PROJECT:
+All field workers with laptops currently use the ArcReader map "TapNCurb" and need a current
+copy of our in-network SQL Server SDE GIS Database with new utilities, ownership, & GPS points.
+They sometimes have spotty internet connection and thus need a reliable disconnected map solution
+to view updated utility and ownership data as soon as new data are available to help them locate
+the City's utilities for Gopher State One Calls.
+
+# OBSTACLES TO OVERCOME:
+The map has normally only been updated every 1-3 months depending on when the field worker connects
+their laptop to the City Network AND Mick Thorstad has updated the database holding all the GIS
+data for the map. The GIS database has been an exported copy of the SQL Server SDE GIS database
+that Richard and Al manage. In the past, Mick has exported all the tables needed for the remote
+database every 1-3 months since it takes him 1-2 days to complete the database export process
+manually; it's a lengthy and annoying manual update process.
+
+# PROPOSED SOLUTION:
+1)	Automate remote database export of GIS database
+2)	Update the batch script that field workers run to copy the new remote database and TapNCurb.pmf
+    map onto their laptops
+3)	Work with IT to add a pop-up message on field worker's laptops that informs them if their
+    database & map is more than 1 week old.
+4)	In the future...Work with IT to possibly "push" the ArcReader map & database updates to remote
+    worker's laptops, so field workers don't need to always bring their laptops into the office.
 """
 
 
@@ -295,7 +318,7 @@ def copySingleFCtoFC(fromGDBpath, toGDBpath, fc):
 #-------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------
 
-# Copy County Assessor’s table
+# Copy County AssessorÂ’s table
 ## table is already downloaded daily, then copy table over to PortabelGDB
 ## function = take copied table & copy into portableGDB
 # Look up data needed in TapNCurb.MXD
@@ -340,7 +363,7 @@ def updateAssessorTable(toGDBpath):
 #-------------------------------------------------------------------------------------------------------
 
  
-# Automate clipping of County'’s Rice Lake townships; copy to new file gdb
+# Automate clipping of County'Â’s Rice Lake townships; copy to new file gdb
 def clipAndCopyRiceLakeFC(toGDB_RiceLake_path = 'S:/GIS_Public/GIS_Data/MapDocuments/Published_Maps/ArcReaderRemoteUpdate/PortableDuluth.gdb/Rice_Lake_Twnshp'):
     """
     PURPOSE:
@@ -701,12 +724,12 @@ portableGIS_ParcelFeatures_path = 'S:/GIS_Public/GIS_Data/MapDocuments/Published
 copySingleFCtoFC(fromGDBpath=arcReaderDefaultGDBPath, toGDBpath=portableGIS_ParcelFeatures_path, fc='Sections_SLC')
 #-------------------------------------------------------------------------------------------------------
 
-# 4. Run function to copy County Assessor’s table into PortableGIS.gdb
+# 4. Run function to copy County AssessorÂ’s table into PortableGIS.gdb
 updateAssessorTable(toGDBpath=portableGISpath)
 
 #-------------------------------------------------------------------------------------------------------
 
-# 5. Run function to clip St. Louis County'’s Rice Lake townships & copy to PortableGIS.gdb
+# 5. Run function to clip St. Louis County'Â’s Rice Lake townships & copy to PortableGIS.gdb
 clipAndCopyRiceLakeFC(toGDB_RiceLake_path = 'S:/GIS_Public/GIS_Data/MapDocuments/Published_Maps/ArcReaderRemoteUpdate/PortableDuluth.gdb/Rice_Lake_Twnshp')
 
 #-------------------------------------------------------------------------------------------------------
@@ -718,8 +741,7 @@ elapsedTimeMinutes = elapsedTimeSeconds/60.0
 
 #------Script End------------------------------------------------------------------------------
 print "----------------------------------------------"
-print "\nScript completed in {0:0.2f} minutes (or {1:.2f} \
-seconds)\nReview database located: {2}".format(elapsedTimeMinutes, elapsedTimeSeconds, arcpy.env.workspace)
+print "\nScript completed in {0:0.2f} minutes (or {1:.2f} seconds)\nReview database located: {2}".format(elapsedTimeMinutes, elapsedTimeSeconds, arcpy.env.workspace)
 print "----------------------------------------------"
 logger.info("\n----------\nScript completed in {0:0.2f} minutes. \nReview database located: {1}.".format(elapsedTimeMinutes, arcpy.env.workspace))
 
